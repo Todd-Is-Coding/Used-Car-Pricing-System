95b351040b20902a436ffb4c8f0e1164e1b9cd00
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-08 18:51:46 +0300

    Applying the interceptor : connecting the current-user interceptor to DI

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,81 +1,88 @@
 import {
-  Body,
-  Controller,
-  Param,
-  Query,
-  Post,
-  Get,
-  Patch,
-  Delete,
-  NotFoundException,
-  Session
+    Body,
+    Controller,
+    Param,
+    Query,
+    Post,
+    Get,
+    Patch,
+    Delete,
+    NotFoundException,
+    Session,
+    UseInterceptors
 } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
-import { CreateUserDto } from './dtos/craete-user.dto';
-import { UpdateUserDTO } from './dtos/update-user.dto';
-import { Serialize } from '../interceptors/serialize.interceptor';
-import { UserDTO } from './dtos/user.dto';
+import {UsersService} from './users.service';
+import {AuthService} from './auth.service';
+import {CreateUserDto} from './dtos/craete-user.dto';
+import {UpdateUserDTO} from './dtos/update-user.dto';
+import {Serialize} from '../interceptors/serialize.interceptor';
+import {UserDTO} from './dtos/user.dto';
+import {CurrentUser} from "./decorators/current-user.decorator";
+import {CurrentUserInterceptor} from "./interceptors/current-user.interceptor";
+import {User} from './users.entity'
 
 @Serialize(UserDTO)
+@UseInterceptors(CurrentUserInterceptor)
 @Controller('auth')
 export class UsersController {
-  constructor(private readonly usersService: UsersService , private readonly authService: AuthService) {}
-
-  @Get('/whoami')
-  whoami(@Session() session : any){
-    return this.usersService.findOne(session.userId);
-  }
-
-  @Post('/signout')
-  signOut(@Session() session : any){
-    session.userId = null;
-  }
-  @Post('/signup')
-  async createUser(@Body() body: CreateUserDto , @Session() session : any) {
-    const  user = await this.authService.signUp(body.email, body.password);
-    session.userId = user.id;
-    return user ;
-  }
-
-  @Post('/signin')
-  async signIn(@Body() body : CreateUserDto , @Session() session : any) {
-    const user = await this.authService.signIn(body.email , body.password)
-    session.userId = user.id;
-    return user ;
-  }
-
-  @Get('/colors/:color')
-  setColor(@Param('color') color: string , @Session() session: any) {
-    session.color = color;
-  }
-
-  @Get('/colors')
-  getColor(@Session() session: any) {
-    return session.color;
-  }
-
-  @Get('/:id')
-  async findUserById(@Param('id') id: string) {
-    const user = await this.usersService.findOne(parseInt(id));
-    if (!user) {
-      throw new NotFoundException('No user has been found');
+    constructor(private readonly usersService: UsersService, private readonly authService: AuthService) {
     }
-    return user;
-  }
 
-  @Get()
-  findAllUsers(@Query('email') email: string) {
-    return this.usersService.find(email);
-  }
+    @Get('/whoami')
+    whoAmI(@CurrentUser() user: User) {
+        return user;
+    }
 
-  @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO) {
-    return this.usersService.update(parseInt(id), body);
-  }
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    }
 
-  @Delete('/:id')
-  removeUser(@Param('id') id: string) {
-    return this.usersService.remove(parseInt(id));
-  }
+    @Post('/signup')
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signUp(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signIn(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signIn(body.email, body.password)
+        session.userId = user.id;
+        return user;
+    }
+
+    @Get('/colors/:color')
+    setColor(@Param('color') color: string, @Session() session: any) {
+        session.color = color;
+    }
+
+    @Get('/colors')
+    getColor(@Session() session: any) {
+        return session.color;
+    }
+
+    @Get('/:id')
+    async findUserById(@Param('id') id: string) {
+        const user = await this.usersService.findOne(parseInt(id));
+        if (!user) {
+            throw new NotFoundException('No user has been found');
+        }
+        return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO) {
+        return this.usersService.update(parseInt(id), body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './users.service';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UsersService} from './users.service';
 import {AuthService} from "./auth.service";
-import { UsersController } from './users.controller';
-import { User } from './users.entity';
+import {UsersController} from './users.controller';
+import {User} from './users.entity';
+import {CurrentUserInterceptor} from "./interceptors/current-user.interceptor";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService, AuthService],
+    imports: [TypeOrmModule.forFeature([User])],
+    controllers: [UsersController],
+    providers: [UsersService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+}
