8343e5a63aacbfbb414b050395d303db58e2a5ef
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-14 10:51:21 +0300

    Updateing : getting typescript to help with mocks

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,13 +2,14 @@ import { Test } from '@nestjs/testing';
 
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
 
 describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
-    const fakeUserService = {
+    const fakeUserService : Partial<UsersService> = {
       find: () => Promise.resolve([]),
       create: (password: string, email: string) =>
-        Promise.resolve({ id: 1, email, password }),
+        Promise.resolve({ id: 1, email, password } as User),
     };
 
     const module = await Test.createTestingModule({
