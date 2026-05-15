88f56eae13f468c42cf782e81ada8f71f5ff7aed
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-15 21:10:19 +0300

    Testing : adding authentication system e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+import { setupApp } from '../src/setup-app';
+
+describe('Authentication system (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    setupApp(app);
+    await app.init();
+  });
+
+  it('handels signup request', () => {
+    const testEmail = `test${Date.now()}@example.com`;
+    
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: testEmail, password: '123456789a' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toBe(testEmail);
+      });
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
