ed446dc868b270d7f684fa03ca5d9210c384a959
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-16 09:23:02 +0300

    Updating : validating create report dto

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,21 +1,38 @@
-export class CreateReportDTO {
-  id!: number;
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLongitude,
+  IsLatitude,
+} from 'class-validator';
 
+export class CreateReportDTO {
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   price!: number;
 
+  @IsString()
   make!: string;
 
+  @IsString()
   model!: string;
 
-  
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
   year!: number;
 
-  
+  @IsLongitude()
   lng!: number;
 
-  
+  @IsLatitude()
   lat!: number;
 
-  
+  @IsNumber()
+  @Min(0)
+  @Max(100000)
   milage!: number;
 }
