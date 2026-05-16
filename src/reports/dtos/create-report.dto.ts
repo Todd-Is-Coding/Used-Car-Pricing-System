cb1438dc1a9b9fee1d0fc3720bb6fab89afe9a01
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-16 09:17:16 +0300

    Creating : implementing create report dto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,21 @@
+export class CreateReportDTO {
+  id!: number;
+
+  price!: number;
+
+  make!: string;
+
+  model!: string;
+
+  
+  year!: number;
+
+  
+  lng!: number;
+
+  
+  lat!: number;
+
+  
+  milage!: number;
+}
