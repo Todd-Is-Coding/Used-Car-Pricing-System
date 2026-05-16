e7add59c6ee9051184d89409098a0318ebd093db
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-05-16 09:33:04 +0300

    Implementing : recieving create report requests

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
-
+import { CreateReportDTO } from './dtos/create-report.dto';
+import { AuthGuard } from '../guards/auth.guard';
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body : CreateReportDTO){
+
+  }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './reports.entity';
+import { CreateReportDTO } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private readonly repo: Repository<Report>,
+  ) {}
+
+  create(reportDTO: CreateReportDTO) {
+    const report = this.repo.create(reportDTO);
+
+    return this.repo.save(report);
+  }
+}
